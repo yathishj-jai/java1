33afa22d13318adced4b776238ad408ebd10c796
MAC <chandangs@Chandans-MacBook-Pro.local>
2025-06-10 06:55:26 +0530

    newfile

---
+++ src/test01/evenodd.java
@@ -0,0 +1,16 @@
+package test01;
+
+public class evenodd {
+
+    public static void main(String args[])
+    {
+        int a=9;
+        if (a%2==0)
+        {
+            System.out.println("the number is even");
+        }else
+        {
+            System.out.println("the number is odd");
+        }
+    }
+}
